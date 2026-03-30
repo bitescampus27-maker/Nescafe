1e577ea099ff20fddc9730f9f77b07b6a5cec675
bitescampus27-maker <bitescampus27@gmail.com>
2026-03-30 21:30:26 +0530

    Update fixOrders.js

--- backend/fixOrders.js
+++ backend/fixOrders.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import orderModel from "./models/orderModel.js";
 import userModel from "./models/userModel.js";
 
-const MONGO_URL = "mongodb+srv://bitescampus27_db_user:campus123@cluster0.amhdb7x.mongodb.net/food-del1";
+const MONGO_URL = "MONGO_URI=mongodb+srv://bitescampus27_db_user:campus123@cluster0.amhdb7x.mongodb.net/food-del1";
 
 const fixOrders = async () => {
   try {
